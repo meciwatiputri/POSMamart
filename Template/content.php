080d75990fa34109adcda4c35deef8aea2716f80
Meci <chacamwp@gmail.com>
2017-07-10 16:46:31 +0700

    templating 2

---
+++ Template/content.php
@@ -0,0 +1,13 @@
+  <div id="page-wrapper">
+            <div class="container-fluid" id="konten">
+                <div class="row" >
+                    <div class="col-lg-12">
+                    </div>
+                </div>
+              
+            </div>
+           
+        </div>
+       
+
+    </div>
