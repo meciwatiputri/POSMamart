80daef768e30209170743e2ad9f6ea074008d178
Meci <chacamwp@gmail.com>
2017-07-11 14:55:27 +0700

    p

---
+++ administrator/template/sidebar.php
@@ -0,0 +1,34 @@
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                    	<li class="panel panel-default">
+                    		<img src="template/img/SITO AZMI NURCHOLIS.jpg" class="img-thumbnail img-responsive " width="100" alt="Ressponsive image">
+                            <p class="">Selamat Datang,<br>Rastika Sadila Rahma<br>
+                            <a href="../login.html"><i class="fa fa-sign-out"></i>Logout</a>
+                    	</li>
+                        <li>
+                            <a href="index2.html" id="beranda"><i class="fa fa-home fa-fw"></i> Beranda</a>
+                        </li>
+                        <li>
+                            <a href="#" id="datapengguna"><i class="fa fa-users fa-fw"></i> Data Pengguna</a>
+                        </li>
+                        <li>
+                            <a href="#" id="databarang"><i class="fa fa-list-alt fa-fw"></i> Data Barang</a>
+                        </li>
+                        <li>
+                            <a href="#" id="datapenjualan"><i class="fa fa-dashboard fa-fw"></i> Data Penjualan</a>
+                        </li>
+                        <li>
+                            <a href="#" id="settings"><i class="fa fa-gear fa-fw"></i> Pengaturan <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="#" id="ubahprofil"><i class="fa fa-user fa-fw"></i> Ubah Profil</a>
+                                </li>
+                                <li>
+                                    <a href="#" id="ubahpassword"><i class="fa fa-gear fa-fw"></i> Ubah Password</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>   
+            </div>
